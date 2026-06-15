0954a9117cd37d14ec5533eb9af564b76cc9452f
Shahed-Alhihi <shahedmousa39@gmail.com>
2026-06-15 23:00:18 +0300

    patient API

--- routes/patients.js
+++ routes/patients.js
@@ -1,11 +1,36 @@
 import express from "express";
+import db from "../db.js";
 
 
-const router = express.Router();
+const router=express.Router();
 
-
-router.get("/", (req, res) => {
-  res.send("Auth route works");
+router.get("/",async(req,res)=>{
+    const result=await db.query("SELECT * FROM patients ORDER BY id");
+    res.json(result.rows);
 });
+
+
+router.get("/:id",async(req,res)=>{
+    const result=await db.query("SELECT * FROM patients WHERE id= $1",[req.params.id,]);
+    res.json(result.rows[0]);
+});
+
+router.post("/", async(req,res)=>{
+    const{
+        name,age,room, condition, admission_date,emergency_contact,image}=req.body;
+
+        const result=await db.query(
+            `INSERT INTO patients(name,age,room, condition, admission_date,emergency_contact,image) 
+            VALUES ($1,$2,$3,$4,$5,$6,$7)
+            RETURNING *`,
+            [name,age,room, condition, admission_date,emergency_contact,image]);
+
+            res.json(result.rows[0]);
+});
+router.delete("/:id", async(req,res)=>{
+    await db.query("DELETE FROM patients WHERE id=$1 ",[req.params.id]);
+    res.json({message:"Patient deleted"});
+});
+
 
 export default router;
